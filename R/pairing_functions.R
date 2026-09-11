b150e0ec864451c55fbe57d95133d4fd152dbb1e
Jekaterīna Butkeviča <jb22035@edu.lu.lv>
2026-09-11 19:21:11 +0300

    upd

---
+++ R/pairing_functions.R
@@ -0,0 +1,36 @@
+# ============================================================
+# pairing_functions.R
+# Functions for generating candidate DBM peak pairs
+# ============================================================
+
+
+# Šeit cenšos vienas Year_plus_Site sērijas ietvaros izveidot visus iespējamos pīķu pārus
+generate_peak_pairs <- function(data) {
+  
+  data <- data %>%
+    arrange(mid_date)
+  
+  pairs <- tidyr::crossing(
+    peak_1 = seq_len(nrow(data)),
+    peak_2 = seq_len(nrow(data))
+  ) %>%
+    filter(
+      peak_2 > peak_1
+    ) %>%
+    mutate(
+      peak_1_date = data$mid_date[peak_1],
+      peak_2_date = data$mid_date[peak_2],
+      
+      generation_days = as.numeric(
+        peak_2_date - peak_1_date
+      ),
+      
+      peak_1_strength = data$peak_strength[peak_1],
+      peak_2_strength = data$peak_strength[peak_2],
+      
+      peak_1_edge = data$observations_to_edge[peak_1],
+      peak_2_edge = data$observations_to_edge[peak_2]
+    )
+  
+  return(pairs)
+}
