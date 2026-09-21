a1e8df2ec353be93484fa14b96ce6e873ff273e8
Jekaterīna Butkeviča <jb22035@edu.lu.lv>
2026-09-21 19:02:08 +0300

    noformējums

--- scripts/05_generate_peak_pairs.R
+++ scripts/05_generate_peak_pairs.R
@@ -1,5 +1,5 @@
 # 05_generate_peak_pairs.R
-# Generate all candidate peak pairs 
+# Generate candidate DBM peak pairs
 
 
 # Packages ----------------------------------------------------
@@ -20,27 +20,7 @@ candidate_peaks <- readRDS(
 source("R/pairing_functions.R")
 
 
-
-# viena serija
-
-test_peaks <- candidate_peaks %>%
-  filter(
-    Year_plus_Site == "2021Dignajas"
-  )
-
-test_pairs <- generate_peak_pairs(
-  test_peaks
-)
-
-test_pairs
-nrow(test_pairs)
-
-
-
-
-
-
-# Generate peak pairs for all series -------------------------------------------
+# Generate all peak pairs -------------------------------------
 
 all_peak_pairs <- candidate_peaks %>%
   group_by(Year_plus_Site) %>%
@@ -49,27 +29,8 @@ all_peak_pairs <- candidate_peaks %>%
   ) %>%
   ungroup()
 
-nrow(all_peak_pairs)
-summary(all_peak_pairs$generation_days)
 
-
-
-all_peak_pairs %>%
-  arrange(generation_days) %>%
-  select(
-    Year_plus_Site,
-    peak_1_date,
-    peak_2_date,
-    generation_days,
-    pair_peak_strength,
-    pair_edge_support
-  ) %>%
-  head(30)
-
-
-
-
-# Broad biological time window -------------------------------------------------
+# Broad biological time window -------------------------------
 
 candidate_generation_pairs <- all_peak_pairs %>%
   filter(
@@ -78,42 +39,7 @@ candidate_generation_pairs <- all_peak_pairs %>%
   )
 
 
-nrow(candidate_generation_pairs)
-summary(candidate_generation_pairs$generation_days)
-
-candidate_generation_pairs %>%
-  arrange(generation_days) %>%
-  select(
-    Year_plus_Site,
-    peak_1_date,
-    peak_2_date,
-    generation_days,
-    pair_peak_strength,
-    pair_edge_support
-  ) %>%
-  print(n = 40)
-
-
-
-candidate_generation_pairs %>%
-  filter(
-    Year_plus_Site == "2021Dignajas"
-  ) %>%
-  select(
-    peak_1,
-    peak_2,
-    generation_days,
-    peak_1_prominence,
-    peak_2_prominence,
-    peak_1_relative_to_max,
-    peak_2_relative_to_max,
-    pair_min_prominence,
-    pair_min_relative_to_max
-  )
-
-
-# Save candidate peak pairs ----------------------------------------------------
-
+# Save --------------------------------------------------------
 
 saveRDS(
   all_peak_pairs,
@@ -127,9 +53,13 @@ saveRDS(
 
 
 if (file.exists("data/processed/all_peak_pairs.rds")) {
-  cat('Fails "data/processed/all_peak_pairs.rds" ir izveidots.\n')
+  cat(
+    'Fails "data/processed/all_peak_pairs.rds" ir izveidots.\n'
+  )
 }
 
 if (file.exists("data/processed/candidate_generation_pairs.rds")) {
-  cat('Fails "data/processed/candidate_generation_pairs.rds" ir izveidots.\n')
+  cat(
+    'Fails "data/processed/candidate_generation_pairs.rds" ir izveidots.\n'
+  )
 }
